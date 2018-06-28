ea88f6212c0efc93fdb324c229ab385e7a6258ec
sam-pop <poplovitch.samuel@gmail.com>
2018-06-27 22:26:25 -0400

    schema.sql syntax fix

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,7 @@ USE burgers_db;
 CREATE TABLE burgers
 (
     id INT AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR
-    (255) NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT 0,
     PRIMARY KEY (id)
 );
