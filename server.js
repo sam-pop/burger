c66bf3a45cbb7db423638c85a179e592cec4beb3
sam-pop <poplovitch.samuel@gmail.com>
2018-06-27 22:45:45 -0400

    MVP up and running!

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const ORM = require('./config/orm');
 
 // Variables
 const PORT = process.env.PORT || 8080;
-let orm = new ORM(); // create new orm instance
+const routes = require('./controllers/burgers_controller');
 const app = express(); // express app init
+const ORM = require('./config/orm');
+let orm = new ORM(); // create new orm instance
 
 
 // body-parser middleware
@@ -16,33 +17,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// set handlebars
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
 // serve static files from the public dir
 app.use(express.static('public'));
 
-
-
-//TODO: delete - for TESTING only
-// app.get('/', function (req, res) {
-//   orm.selectAll('burgers', function (data) {
-//     console.log(data);
-//   });
-//   res.status(200).end();
-// });
-// app.get('/', function (req, res) {
-//     orm.insertOne('burgers','burger_name','test-burger', function (data) {
-//         console.log(data.insertId);
-//     });
-//     res.status(200).end();
-// });
-// app.get('/', function (req, res) {
-//     orm.updateOne('burgers','devoured','true',2, function (data) {
-//         console.log(data);
-//     });
-//     res.status(200).end();
-// });
-
-
-
+app.use(routes);
 
 // start listening on specified port
 app.listen(PORT, function () {
