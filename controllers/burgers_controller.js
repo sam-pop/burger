cdf673bb6922359a2cde98299cf32f2ce0853afe
sam-pop <poplovitch.samuel@gmail.com>
2018-06-27 22:37:05 -0400

    router + routes updated in controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const Burger = require('../models/burger');
+
+let router = express.Router();
+let burger = new Burger();
+
+router.get('/', function (req, res) {
+    burger.selectAll(function (data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post('/api/burgers', function (req, res) {
+    burger.insertOne(req.body.name, function (result) {
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+
+router.put('/api/burgers/:id', function (req, res) {
+    burger.updateOne(req.params.id, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
